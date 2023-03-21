6b53a24c758a6f1ba91c5d22f200aa5b52bce573
boobalannaresh <bestforevermvi@gmail.com>
2023-03-21 23:39:38 +0530

    first commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const env = {
+    // api : "http://localhost:4000/auth",
+    // api1 : "http://localhost:4000/api",
+    // redirect : "http://localhost:4000/api/shortUrl"
+
+    api : "https://url-shortener-backend-lovat.vercel.app/auth",
+    api1 : "https://url-shortener-backend-lovat.vercel.app/api",
+    redirect : "https://url-shortener-backend-lovat.vercel.app/api/shortUrl"
+    
+    
+}
